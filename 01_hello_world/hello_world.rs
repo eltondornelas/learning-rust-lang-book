8ac5f1c200341ccc489335c1b7a66eb7a927eaae
Elton Dornelas <eltondornelas@gmail.com>
2021-03-14 21:47:49 -0300

    new adjust in gitignore and hello_world module

--- 01_hello_world/hello_world.rs
+++ 01_hello_world/hello_world.rs
@@ -1,4 +1,7 @@
 fn main() {
-    println!("hellow world! rustaceans!");    
+    println!("hello world! rustaceans!");
 
+    // this module was created without cargo
+    // $ rustc hello_world.rs  // to build
+    // $ ./hello_world // to run
 }
