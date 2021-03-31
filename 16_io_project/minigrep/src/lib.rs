480a4baab236efb07c11b8ce79aace5996e11fbe
Elton Dornelas <eltondornelas@gmail.com>
2021-03-31 19:01:45 -0300

    adding the search method and test

--- 16_io_project/minigrep/src/lib.rs
+++ 16_io_project/minigrep/src/lib.rs
@@ -16,13 +16,53 @@ impl Config {
         let filename = args[2].clone();
 
         Ok(Config { query, filename })
+
+        // $ cargo build
+        // $ cargo run frog poem.txt
+        // $ cargo run body poem.txt
+        // $ cargo run monomorphization poem.txt
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
-
+    // println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        // .lines() return an iterator handling line-by-line
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
+/* test module
+*********************************************************************************/
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        // let contents = "Rust:\nsafe, fast, productive.\nPick three.";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
