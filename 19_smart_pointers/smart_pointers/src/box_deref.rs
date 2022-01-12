2ca981221f6d0b32d0c3f7e4314a2acbcb1ae982
Elton Dornelas <eltondornelas@gmail.com>
2022-01-11 21:51:53 -0300

    adding module 19_smart_pointers - using Box<T> - creating a custom MyBox<T> - Deref trait

---
+++ 19_smart_pointers/smart_pointers/src/box_deref.rs
@@ -0,0 +1,48 @@
+use std::ops::Deref;
+
+use crate::box_deref::List::{Cons, Nil};
+
+enum List {
+    // Cons(i32, List),
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+pub fn mybox_deref_main() {
+    let b = Box::new(5);
+    println!("b = {}", b);
+
+    // let list = Cons(1, Cons(2, Cons(3, Nil)));
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = 5;
+    // let y = &x;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    // assert_eq!(5, y);  // error
+
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
---
+++ 19_smart_pointers/smart_pointers/src/main.rs
@@ -0,0 +1,7 @@
+use crate::box_deref::mybox_deref_main;
+
+mod box_deref;
+
+fn main() {
+    // mybox_deref_main()
+}
